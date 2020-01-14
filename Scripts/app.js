6d7f46dab908720aa6fa50661b8fee07416434da
Your Name <you@example.com>
2020-01-14 19:59:28 +1000

    initial message

---
+++ Scripts/app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+
+//self -executing anoymous function
+//IIFE -- Immediately Invoked Function Expression
+(function(){
+let myFunctionalVariable = 0;
+
+    function Start(){
+        let myLocalVariable = 0;
+        console.log(`App started...${myFunctionalVariable}`);
+
+    }
+    window.addEventListener("load",Start);
+
+
+
+})();
